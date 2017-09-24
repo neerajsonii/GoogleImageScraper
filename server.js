5e7b7a55486b7a6102b80a2651e6bf72f3a951f2
neerajsoni <nrsoni1992@gmail.com>
2017-09-24 18:47:01 +0530

    heroku push

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app = express();
 var origins = ["http://localhost:3100", "http://localhost:3000"];
 
 app.get('/*', function(req, res, next) {
-    origins.forEach(function(val, key) {
-        res.header('Access-Control-Allow-Origin', val);
-    })
+    //origins.forEach(function(val, key) {
+        res.header('Access-Control-Allow-Origin', '*');
+    //})
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.header('Access-Control-Allow-Credentials', true);
